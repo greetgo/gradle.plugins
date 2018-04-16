40ca6163987688ca18d5b8a829867b3c42512a44
ekolpakov <ekolpakov@greet-go.com>
2018-04-16 16:39:03 +0600

    Added GG_REPO

--- src/main/groovy/kz/greetgo/gradle/plugins/Env.groovy
+++ src/main/groovy/kz/greetgo/gradle/plugins/Env.groovy
@@ -9,9 +9,12 @@ class Env {
   }
 
   static final String GG_REPO_URL = 'GG_REPO_URL'
+  static final String GG_REPO = 'GG_REPO'
 
   static String ggRepoUrl() {
-    return getEnv(GG_REPO_URL)
+    def url1 = getEnv(GG_REPO_URL)
+    if (url1 != null) return url1
+    return getEnv(GG_REPO)
   }
 
   static final String GG_REPO_USERNAME = 'GG_REPO_USERNAME'
@@ -21,26 +24,31 @@ class Env {
   }
 
   static final String GG_REPO_PASSWORD = 'GG_REPO_PASSWORD'
+
   static String ggRepoPassword() {
     return getEnv(GG_REPO_PASSWORD)
   }
 
   static final String LIB_SIGN_GPG_KEY_ID = 'LIB_SIGN_GPG_KEY_ID'
+
   static String signGpgKeyId() {
     return getEnv(LIB_SIGN_GPG_KEY_ID);
   }
 
   static final String LIB_SIGN_GPG_KEY_PASSWORD = 'LIB_SIGN_GPG_KEY_PASSWORD'
+
   static String signGpgKeyPassword() {
     return getEnv(LIB_SIGN_GPG_KEY_PASSWORD);
   }
 
   static final String LIB_SIGN_GPG_KEY_LOCATION = 'LIB_SIGN_GPG_KEY_LOCATION'
+
   static String signGpgKeyLocation() {
     return getEnv(LIB_SIGN_GPG_KEY_LOCATION);
   }
 
   static final String LIB_SONATYPE_ACCOUNT_HASH_ID = 'LIB_SONATYPE_ACCOUNT_HASH_ID'
+
   static String sonatypeAccountId() {
     return getEnv(LIB_SONATYPE_ACCOUNT_HASH_ID);
   }
